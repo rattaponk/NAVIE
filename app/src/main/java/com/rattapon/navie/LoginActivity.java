b3c8e8513d06bec5576e61e35a48adfb53db7a8c
rattapon <rattapon.kaewpinjai@gmail.com>
2017-11-12 01:22:02 +0700

    + FirebaseAuth + FirebaseRealtimeDB + Ripper effect

--- app/src/main/java/com/rattapon/navie/LoginActivity.java
+++ app/src/main/java/com/rattapon/navie/LoginActivity.java
@@ -1,13 +1,115 @@
 package com.rattapon.navie;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btLogin;
+    private TextView tvCreateNewAcount;
+    private TextView tvForgotPassword;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initInstances();
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+//        currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
+    private void initInstances() {
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btLogin = findViewById(R.id.bt_login);
+        tvCreateNewAcount = findViewById(R.id.tv_create);
+        tvForgotPassword = findViewById(R.id.tv_forgot);
+
+        btLogin.setOnClickListener(this);
+        tvCreateNewAcount.setOnClickListener(this);
+        tvForgotPassword.setOnClickListener(this);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+//            // Name, email address, and profile photo Url
+//            String name = currentUser.getDisplayName();
+//            String email = currentUser.getEmail();
+//            Uri photoUrl = currentUser.getPhotoUrl();
+//
+//            // Check if user's email is verified
+//            boolean emailVerified = currentUser.isEmailVerified();
+//
+//            // The user's ID, unique to the Firebase project. Do NOT use this value to
+//            // authenticate with your backend server, if you have one. Use
+//            // FirebaseUser.getToken() instead.
+//            String uid = currentUser.getUid();
+            startActivity(new Intent(this, MapActivity.class));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btLogin) {
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+            if (!email.isEmpty() && !password.isEmpty()) {
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    currentUser = mAuth.getCurrentUser();
+                                    updateUI(currentUser);
+                                    Toast.makeText(LoginActivity.this, "Login success with " + currentUser.getEmail(), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Login failed.\n" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
+            } else
+                Toast.makeText(LoginActivity.this, "Please enter email and password.", Toast.LENGTH_SHORT).show();
+
+//            etEmail.getText().clear();
+            etPassword.getText().clear();
+        } else if (view == tvCreateNewAcount) {
+            startActivity(new Intent(this, RegisterActivity.class));
+        } else if (view == tvForgotPassword) {
+
+        }
+    }
+
 }
+
+
--- app/src/main/java/com/rattapon/navie/RegisterActivity.java
+++ app/src/main/java/com/rattapon/navie/RegisterActivity.java
@@ -1,43 +1,220 @@
 package com.rattapon.navie;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int mDay , mMonth, mYear;
+    private int mDay, mMonth, mYear;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etConPass;
+    private EditText etName;
+    private RadioGroup rgGender;
     private EditText etDOB;
+    private Button btRegister;
+
+    private FirebaseAuth mAuth;
+
+    private class GenericTextWatcher implements TextWatcher {
+        private View view;
+
+        private GenericTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            String text = editable.toString();
+            if (view == etEmail) {
+                if (!isEmailValid(text)) {
+                    etEmail.setError("Invalid Email Address.");
+                }
+            } else if (view == etPassword) {
+                if (!isPasswordValid(text)) {
+                    etPassword.setError("Required more 8 characters.");
+                }
+            } else if (view == etConPass) {
+                if (!isSamePassword()) {
+                    etConPass.setError("Password not match.");
+                }
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        etDOB = (EditText)findViewById(R.id.et_dob);
+        initInstance();
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void initInstance() {
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etConPass = findViewById(R.id.et_cpassword);
+        etName = findViewById(R.id.et_name);
+        etDOB = findViewById(R.id.et_dob);
+        rgGender = findViewById(R.id.rg_gender);
+        btRegister = findViewById(R.id.bt_register);
+
         etDOB.setInputType(InputType.TYPE_NULL);
 
-        etDOB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                        month = month+1;
-                        etDOB.setText(day + "/"+ month + "/" + year);
-                        mDay = day;
-                        mMonth = month;
-                        mYear = year;
-                    }
-                }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-            }
-        });
+        btRegister.setOnClickListener(this);
+        etDOB.setOnClickListener(this);
 
+        etEmail.addTextChangedListener(new GenericTextWatcher(etEmail));
+        etPassword.addTextChangedListener(new GenericTextWatcher(etPassword));
+        etConPass.addTextChangedListener(new GenericTextWatcher(etConPass));
     }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+//            // Name, email address, and profile photo Url
+//            String name = currentUser.getDisplayName();
+//            String email = currentUser.getEmail();
+//            Uri photoUrl = currentUser.getPhotoUrl();
+//
+//            // Check if user's email is verified
+//            boolean emailVerified = currentUser.isEmailVerified();
+//
+//            // The user's ID, unique to the Firebase project. Do NOT use this value to
+//            // authenticate with your backend server, if you have one. Use
+//            // FirebaseUser.getToken() instead.
+//            String uid = currentUser.getUid();
+            startActivity(new Intent(this, MapActivity.class));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btRegister) {
+            String btgender = "";
+            switch (rgGender.getCheckedRadioButtonId()) {
+                case R.id.rb_male:
+                    btgender = "Male";
+                    break;
+                case R.id.rb_female:
+                    btgender = "Female";
+                    break;
+                default:
+                    break;
+            }
+
+            final String email = etEmail.getText().toString();
+            final String password = etPassword.getText().toString();
+            final String conpass = etConPass.getText().toString();
+            final String name = etName.getText().toString();
+            final String dob = mDay + "/" + mMonth + "/" + mYear;
+            final String gender = btgender;
+
+            if (isEmailValid(email) && isPasswordValid(password) && isSamePassword() && !name.isEmpty() && !dob.isEmpty()) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(RegisterActivity.this, "Authentication success.", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    pushRegisterData(user, email, name, gender, dob);
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.\n" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
+//            Toast.makeText(this, email + "\n" + password + "\n" + name + "\n" + gender + "\n" + dob, Toast.LENGTH_SHORT).show();
+            } else
+                Toast.makeText(RegisterActivity.this, "Please enter your information.", Toast.LENGTH_SHORT).show();
+
+        } else if (view == etDOB) {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    month = month + 1;
+                    etDOB.setText(day + "/" + month + "/" + year);
+                    mDay = day;
+                    mMonth = month;
+                    mYear = year;
+                }
+            }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+    }
+
+    public static boolean isEmailValid(String email) {
+        String VALID_EMAIL_ADDRESS_REGEX = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(VALID_EMAIL_ADDRESS_REGEX, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.find();
+    }
+
+    public static boolean isPasswordValid(String password) {
+//        ^                 # start-of-string
+//        (?=.*[0-9])       # a digit must occur at least once
+//        (?=.*[a-z])       # a lower case letter must occur at least once
+//        (?=\S+$)          # no whitespace allowed in the entire string
+//        .{8,}             # anything, at least eight places though
+//        $                 # end-of-string
+//        String VALID_PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{8,}$";
+        String VALID_PASSWORD_REGEX = "^(?=\\S+$).{8,}$";
+        Pattern pattern = Pattern.compile(VALID_PASSWORD_REGEX, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+    }
+
+    public boolean isSamePassword() {
+        String conpass = etConPass.getText().toString();
+        String password = etPassword.getText().toString();
+        if (conpass.equals(password)) {
+            return true;
+        } else
+            return false;
+    }
+
+    public void pushRegisterData(FirebaseUser currentUser,String e,String p, String g, String dob) {
+        String key = currentUser.getUid();
+        DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mUsersRef = mRootRef.child("users");
+        User user = new User(e,p,g,dob);
+        mUsersRef.child(key).setValue(user);
+    }
+
 }
